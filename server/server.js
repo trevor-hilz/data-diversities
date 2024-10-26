b81dbf03858d62dbe319f6f0ef6bd5a9792b5c91
Trevor <trevorhilz@gmail.com>
2024-10-26 15:25:34 -0500

    CORS - added app.opions

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const corsOptions = {
   ],
   optionSuccessStatus: 200,
 };
-
+app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, '../dist')));
